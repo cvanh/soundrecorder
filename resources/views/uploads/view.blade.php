750ab8d2c9056340566f80a3cf3e9d88252e3bf5
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2024-03-20 18:28:20 +0100

    related reports now link

--- resources/views/uploads/view.blade.php
+++ resources/views/uploads/view.blade.php
@@ -1,14 +1,24 @@
-<x-header/>
-<h1>report #{{$report->id}} info:</h1>
+<x-header />
+<h1>report #{{ $report->id }} info:</h1>
 @foreach ($report as $key => $node)
     <li>{{ $key }}: {{ $node }}</li>
 @endforeach
 
 <div>
     <h2>other reports on this date:</h2>
-@foreach ($related as $relatedReport)
-    <div>
-        {{$relatedReport->type}}
-    </div>    
-@endforeach
+    @foreach ($related as $relatedReport)
+        <a href="{{ route('uploads.view', ['id' => $relatedReport->id]) }}">
+            <div style="height: 100px; overflow:hidden; border: 1px solid black;">
+                <div>
+                    #{{ $relatedReport->id }}
+                </div>
+                <div>
+                    type: {{ $relatedReport->type }}
+                </div>
+                <div>
+                    time: {{ $relatedReport->created_at }}
+                </div>
+            </div>
+        </a>
+    @endforeach
 </div>
